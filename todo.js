f18cee2b020fdabb6ea3a469142047e90ab1cb53
beatsbydan <danielonyeachonam2020@gmail.com>
2022-09-07 16:05:25 +0100

    final work

--- todo.js
+++ todo.js
@@ -1,22 +1,62 @@
-//changing the theme via icons
-//getting light theme
-const light = document.querySelector('.light')
-//changing to light theme
-function lightTheme(){
-    document.body.classList.remove('dark__theme')
-    light.classList.remove('in-light__mode')
-    dark.classList.remove('in-dark__mode')
-}
-//gettigng dark theme
-const dark = document.querySelector('.dark')
-//changing to dark theme
-function darkTheme(){
-    document.body.classList.add('dark__theme')
+//CHANGING THE THEMES
+
+//creating the local storage for the dark theme alone
+let darkThemeStorage = localStorage.getItem ("darkTheme")
+
+//getting light and dark theme buttons
+const light = document.querySelector(".light")
+const dark = document.querySelector(".dark")
+
+//function to enable the dark theme
+const enableDark = () => { 
     light.classList.add('in-light__mode')
     dark.classList.add('in-dark__mode')
+    document.body.classList.add('dark__theme')
+    
+    //setting the content of the local storage for the dark theme to enabled
+    localStorage.setItem("darkTheme", "enabled")
 }
 
+
+//function to disable the dark theme
+const disableDark = () => {
+    dark.classList.remove('in-dark__mode')
+    light.classList.remove('in-light__mode')
+    document.body.classList.remove('dark__theme')
+    
+    //setting the content of the local storage for the dark theme to null
+    localStorage.setItem("darkTheme", null)
+}
+
+
+//condition to always keep dark theme enabled, even on refresh when enabled
+if(darkThemeStorage === "enabled"){
+    enableDark();
+}
+
+//function to change theme on click
+function changeTheme(){
+    //getting the property of dark theme in the local storage first before the test
+    darkThemeStorage = localStorage.getItem("darkTheme") 
+    
+    //if it isn't enabled, we enable it 
+    if(darkThemeStorage !== "enabled"){
+        enableDark();
+    }
+    
+    //if it is enabled, we'd disable it
+    else{
+        disableDark();
+    }
+}
+
+
+
 //TODO APP
+
+//dynamic number
+let dynamicNumber =document.querySelector("#dynamic")
+
 //getting the input of the lists
 const toDoInput = document.querySelector('.todoInput')
 
@@ -33,19 +73,24 @@ const arrayA = document.querySelectorAll((".flex__a p"))
 const arrayB = document.querySelectorAll((".flex__b p"))
 
 //functions for all, active and completed
+//for the desktop
 arrayA.forEach((list) => {
     list.addEventListener('click', () => { 
         document.querySelector("p.current--Array").classList.remove("current--Array")
         list.classList.add("current--Array")
-        //shows the array containing the id of either all, active or completed lists
+        
+        //shows the array containing the id of either all, active or completed lists equal to the statuses of each todo item
         showToDoList(list.id)
     })
 })
+
+//for the mobile
 arrayB.forEach((list) => {
     list.addEventListener('click', () => { 
         document.querySelector("p.current--Arr").classList.remove("current--Arr")
         list.classList.add("current--Arr")
-        //shows the array containing the id of either all, active or completed lists
+        
+        //shows the array containing the id of either all, active or completed lists equal to the statuses of each todo item
         showToDoList(list.id)
     })
 })
@@ -54,15 +99,23 @@ arrayB.forEach((list) => {
 
 //function to show each todo list of either all, active or completed
 function showToDoList(filteredList){
+    
+    //declaring the list item to be appended
     let li = "";
+    
+    //if the todolistarray exists, we build the lists this way
     if(toDoListArray){
-         //for each todo item added, we want to display on the screen
+        
+        //for each todo item added, we want to display on the screen, each of those lists
         toDoListArray.forEach((todo, id) => {
+            
             //to keep the checked class constant even upon refresh, using the ternary operator
             let todoIsChecked = todo.status == "completed" ? "checked" : ""
             
+            //condition for the statuses. This would display the array of either all, active or completed
             if(filteredList === todo.status || filteredList === "all"){
-                //what should be displayed
+                
+                //what should be displayed; appending li
                 li += `<li class="list-group-item" draggable="true">
                             <div for="${id}" class="box">
                                 <input type="checkbox" onclick=" return isChecked(this)" id= ${id}  ${todoIsChecked} data-checkbox class="checkbox">
@@ -74,46 +127,56 @@ function showToDoList(filteredList){
                         </li>`
                 }
         })
-        //dynamic number
-        let dynamicNumber =document.querySelector("#dynamic")
+        
         //setting the dynamic number to be equal to the filtered array
-        dynamicNumber.innerHTML=toDoListArray.filter((complete)=>{
+        dynamicNumber.innerHTML = toDoListArray.filter((complete)=>{
             return complete.status === "active"
         }).length
     }
+
     //setting the content of the unordered list to the list items
     toDoContainer.innerHTML = li || `<p class ="default-statement">Your list is empty, Add a Task!</p>`;
 }
+
 //showing the entire list of todo-items by default
 showToDoList("all");
 
 //function to remove each item
 function removeItem(idToBeDeleted){
+    
     //removing the selected item;
     toDoListArray.splice(idToBeDeleted, 1)
+    
     //parsing into the local storage
     localStorage.setItem("list", JSON.stringify(toDoListArray))
+    
+    //showing the current lists after item(s) have been deleted
     showToDoList("all")
 }
 
 //function to display the checks
 function isChecked(todo){
+    
     //getting the todo item class so as tobe able to edit
     let selectedTodo = todo.nextElementSibling
+    
     //statement to add / remove the checked class when checkbox is active or inactive
     if(todo.checked){
         selectedTodo.classList.add("checked")
+        
         //updating the status
         toDoListArray[todo.id].status = "completed"
     }
     else{
         selectedTodo.classList.remove("checked")
+        
+        //updating the status
         toDoListArray[todo.id].status = "active"
     }
-    //condition to always return the number active items available
+
+    //condition to always return the number of active items available and append to the dynamic number
     if(todo.checked || !todo.checked){
-        let dynamicNumber =document.querySelector("#dynamic")
-        dynamicNumber.innerHTML=toDoListArray.filter((complete)=>{
+        dynamicNumber.innerHTML = toDoListArray.filter((complete)=>{
             return complete.status === "active"
         }).length
     }
@@ -124,22 +187,30 @@ function isChecked(todo){
 
 //adding an event listener to the input to read its content
 toDoInput.addEventListener("keyup", e => {
+    
     //obtaining the value of the todo that is about to be created
     let toDo = toDoInput.value.trim();
+    
     //statement to add to the local storage when the key "enter" is hit
     if(e.key == "Enter" && toDo){
+        
         //statement to parse an empty array in if there is no local storage available
         if(!toDoListArray){
             toDoListArray = [];
         }
+        
         //setting the value of the input to null after enter is pressed
         toDoInput.value = "";
+        
         //creating the object that'd contain the value obtained from the input
         let whatToDo = {name: toDo, id: Math.random(), status: 'active'}
+        
         //pushing each todo item to the local storage
         toDoListArray.push(whatToDo)
+        
         //setting items in the local storage
         localStorage.setItem("list", JSON.stringify(toDoListArray))
+        
         //showing the todo lis items on the screen
         showToDoList("all");
     }
@@ -148,46 +219,59 @@ toDoInput.addEventListener("keyup", e => {
 //clearing completed
 const clearBtn = document.querySelector(".clear__completed p")
 clearBtn.addEventListener("click", () => {
+    
     //removing the selected item;
     toDoListArray = toDoListArray.filter(done => {
         return done.status === "active"
     })
+    
     //parsing into the local storage
     localStorage.setItem("list", JSON.stringify(toDoListArray))
+    
+    //changing the display back to "all"
     document.querySelector("p.current--Array").classList.remove("current--Array")
     arrayA[0].classList.add("current--Array")
     document.querySelector("p.current--Arr").classList.remove("current--Arr")
     arrayB[0].classList.add("current--Arr")
+    
+    //showing the list of "all"
     showToDoList("all")
 })
 
 //adding the draggable function
 //getting the list items
 const draggables =  document.querySelectorAll(".list-group-item")
-//getting the drag area
+
+//getting the drag area which is the list of the containers
 const dragArea = document.getElementById("lists")
+    //function to swap array elements around with their ids
     function swapArrayElements(arr, indexA, indexB) {
         var temp = arr[indexA];
         arr[indexA] = arr[indexB];
         arr[indexB] = temp;
     };
     
+    //function to order the order each new list
     function orderList(oldIndex, newIndex) {
         swapArrayElements(toDoListArray, oldIndex, newIndex)
-    
+        
+        //saving to the local storage
         localStorage.setItem("list", JSON.stringify(toDoListArray));
     }
     
     
     // For sorting the list
+    //using sortable JS(from https://cdnjs.com)
     Sortable.create(dragArea, {
-        animation: 100,
+        animation: 350,
         group: 'list-1',
         draggable: '.list-group-item',
         handle: '.list-group-item',
         sort: true,
         filter: '.sortable-disabled',
         chosenClass: 'active',
+
+        //ordering the list
         onSort: function (/*Event*/evt) {
         orderList(evt.oldIndex, evt.newIndex);
     },
@@ -195,65 +279,7 @@ const dragArea = document.getElementById("lists")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*new Sortable(dragArea, {
-    animation: 150,
-    ghostClass: 'sortable-ghost'
-});*/    
-/*
+/*alternate function for the dragging
 //event listeners for each draggable
 draggables.forEach((draggable)=>{
     //when the drag starts
